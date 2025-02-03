130ca1ece0adb3f1ab6c2e2ab25cae99274a6d95
TorenIvan <Vaggelisshmos@gmail.com>
2025-02-03 16:39:46 +0200

    Fixing skeleton view to be responsive

--- src/components/SkeletonList.component.vue
+++ src/components/SkeletonList.component.vue
@@ -1,12 +1,15 @@
 <template>
-  <ul v-if="isVisible" class="list bg-spotify-dark rounded-box shadow-md">
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
-    <li class="list-row skeleton w-130 h-25 bg-spotify-gray"></li>
+  <ul
+    v-if="isVisible"
+    class="list bg-spotify-dark rounded-box shadow-md p-4 w-full max-w-2xl md:min-w-128 mx-auto"
+  >
+    <li
+      class="border-b py-3 grid grid-cols-[2fr_auto] gap-y-2 w-full h-25 justify-around"
+      v-for="index in 7"
+      :key="index"
+    >
+      <div class="skeleton w-full h-full bg-spotify-gray rounded-md"></div>
+    </li>
   </ul>
 </template>
 
